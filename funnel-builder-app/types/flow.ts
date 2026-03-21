2bf7c2aa625e8eac6831ad59f2bb2f99a61d66b3
Arthur Alonso <arthuralonso8356@gmail.com>
2026-03-21 16:20:09 -0300

    :sparkles: feat: Adicionando Node Types (Ad, Landing, Form, Checkout)

---
+++ funnel-builder-app/types/flow.ts
@@ -0,0 +1,23 @@
+export type AdNodeData = {
+    label: string
+    impressions: number
+    clicks: number
+}
+
+export type LandingNodeData = {
+    label: string
+    visits: number
+    leads: number
+}
+
+export type FormNodeData = {
+    label:string
+    submissions: number
+    registrations: number
+}
+
+export type CheckoutNodeData = {
+    label:string
+    started: number
+    purchases: number
+}
