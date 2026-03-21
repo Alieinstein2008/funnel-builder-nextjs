de283bfd73c120f1a34801f76b7612e9f7ffe484
Arthur Alonso <arthuralonso8356@gmail.com>
2026-03-21 16:52:33 -0300

    :recycle: refactor: Alterando FlowCanvas adicionando os componentes estilizados

--- funnel-builder-app/components/flow/FlowCanvas.tsx
+++ funnel-builder-app/components/flow/FlowCanvas.tsx
@@ -1,27 +1,78 @@
 "use client"
 
 import React from "react"
+
 import ReactFlow, {
     Background,
     Controls,
     MiniMap,
 } from "reactflow"
 
-const initialNodes = [
+import AdNode from "./nodes/AdNode"
+import LandingNode from "./nodes/LandingNode"
+import FormNode from "./nodes/FormNode"
+import CheckoutNode from "./nodes/CheckoutNode"
+
+const nodeTypes = {
+    ad: AdNode,
+    land: LandingNode,
+    form: FormNode,
+    check: CheckoutNode
+}
+
+const nodes = [
     {
         id: "1",
+        type: "ad",
         position: { x: 100, y: 100 },
-        data: { label: "Anúncio" },
-        type: "default",
+        data: {
+            label: "Anúncio",
+            impressions: 5000,
+            clicks: 800
+        }
     },
+    {
+        id: "2",
+        type: "land",
+        position: { x: 350, y: 100 },
+        data: {
+            label: "Landing Page",
+            visits: 800,
+            leads: 320
+        }
+    },
+    {
+        id: "3",
+        type: "form",
+        position: { x: 600, y: 100 },
+        data: {
+            label: "Formulário",
+            registrations: 150,
+            submissions: 320
+        }
+    },
+    {
+        id: "4",
+        type: "check",
+        position: { x: 850, y: 100 },
+        data: {
+            label: "Checkout",
+            started: 150,
+            purchases: 320
+        }
+    },
+
+
+
 ]
 
-const initialEdges: any[] = []
+const edges: any[] = []
+
 
 export default function FlowCanvas() {
     return (
         <div className="w-full h-screen">
-            <ReactFlow nodes={initialNodes} edges={initialEdges}>
+            <ReactFlow nodes={nodes} edges={edges} nodeTypes={nodeTypes}>
                 <Background />
                 <Controls />
                 <MiniMap />
