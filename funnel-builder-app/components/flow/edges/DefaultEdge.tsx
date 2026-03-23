ec88dae6202701c6b6f52c6acbb6e5c0ec4cc8fb
Arthur Alonso <arthuralonso8356@gmail.com>
2026-03-23 20:42:45 -0300

    :lipstick: feat: Adicionando visual padrão da Edge do projeto

---
+++ funnel-builder-app/components/flow/edges/DefaultEdge.tsx
@@ -0,0 +1,30 @@
+import { BaseEdge, EdgeProps, getBezierPath } from 'reactflow'
+
+export default function DefaultEdge({
+  id,
+  sourceX,
+  sourceY,
+  targetX,
+  targetY,
+  sourcePosition,
+  targetPosition,
+  style = {},
+  markerEnd,
+}: EdgeProps) {
+  const [edgePath] = getBezierPath({
+    sourceX,
+    sourceY,
+    sourcePosition,
+    targetX,
+    targetY,
+    targetPosition,
+  })
+
+  return (
+    <BaseEdge
+      path={edgePath}
+      markerEnd={markerEnd}
+      style={style}
+    />
+  )
+}
