b60581b2a8f71c5d8496e111c643613fd5eaddd9
Arthur Alonso <arthuralonso8356@gmail.com>
2026-03-23 23:51:28 -0300

    :wrench: chore: Types e Interfaces do utilizados no projeto

--- funnel-builder-app/types/flow.ts
+++ funnel-builder-app/types/flow.ts
@@ -1,3 +1,5 @@
+import type { Node, Position } from "reactflow"
+
 export type AdNodeData = {
     label: string
     impressions: number
@@ -11,13 +13,45 @@ export type LandingNodeData = {
 }
 
 export type FormNodeData = {
-    label:string
+    label: string
     submissions: number
     registrations: number
 }
 
 export type CheckoutNodeData = {
-    label:string
+    label: string
     started: number
     purchases: number
 }
+
+export type CustomNodeData = {
+    label: string
+    firstStatName: string
+    secondStatName: string
+    firstStatValue: number
+    secondStatValue: number
+    color: "blue" | "green" | "red" | "yellow" | "purple" | "pink" | "indigo" | "gray"
+    icon: string
+}
+
+export interface CustomNodeDialogProps {
+    isOpen: boolean
+    onClose: () => void
+}
+
+export type NodeEditorProps = {
+    selectedNode: FlowNode | null
+    onClose: () => void
+    onSave: (nodeId: string, data: any) => void
+    onDelete: (nodeId: string) => void
+}
+
+export type CustomHandleProps = {
+  type: "source" | "target"
+  position: Position
+  color: string
+}
+
+export type FlowNode = Node<AdNodeData> | Node<LandingNodeData> | Node<FormNodeData> | Node<CheckoutNodeData> | Node<CustomNodeData>
+
+export type NodeType = "ad" | "landing" | "form" | "checkout" | "custom"
